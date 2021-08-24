51c8415a6bff54ed4db4b394af49b5d9cfdda213
Matt Firmston <mattfirmston@gmail.com>
2021-08-24 17:20:19 +0100

    potential partial fix to likes

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   const posts = document.querySelectorAll('.post');
   posts.forEach((post) => {
-    post.querySelector('.like-button').addEventListener('click', (e) => {
-      testing = post.querySelector(".button_to")
-      token = testing.querySelector("input[name=authenticity_token").value; 
-      e.preventDefault();
+    post.querySelector('.like-button').addEventListener('click', (event) => { 
+      event.preventDefault();
+      // testing = post.querySelector(".button_to")
+      token = post.querySelector("input[name=authenticity_token]").value;
       fetch(`posts/${post.id}/likes`, {
         method: 'POST',
         credentials: 'same-origin',
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
       .then(text => {
         const parser = new DOMParser();
         const htmlDocument = parser.parseFromString(text, "text/html");
-        const updatedLike = htmlDocument.documentElement.querySelector(".likes").innerHTML;
+        console.log(htmlDocument.body.div)
+        const updatedLike = htmlDocument.documentElement.querySelector(`ID${post.id}`).innerHTML;
         post.querySelector('.likes').innerHTML = updatedLike;
       })
       });
