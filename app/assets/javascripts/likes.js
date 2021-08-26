1924e5ce21a792d1bc8bb6426bda3690a74d1b97
karimabuseer <karimabuseer@gmail.com>
2021-08-26 17:12:35 +0100

    Added async reload to like and unlike buttons

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const posts = document.querySelectorAll('.post');
+
   posts.forEach((post) => {
-    post.querySelector('.like-button').addEventListener('click', (event) => { 
+    if (post.querySelector('.like-button') !== null) { 
+      likingStuff(post)
+    } else { 
+      unlikingStuff(post)
+    }
+  });
+
+  function likingStuff(post) {
+    let likeButton = post.querySelector(`.like-button`)
+    likeButton.addEventListener('click', (event) => {
       event.preventDefault();
-      // testing = post.querySelector(".button_to")
       token = post.querySelector("input[name=authenticity_token]").value;
-      fetch(`posts/${post.id}/likes`, {
+      fetch(`posts/${post.id}/likes/`, {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
@@ -20,6 +29,42 @@ document.addEventListener('DOMContentLoaded', () => {
         updatedLike = htmlDocument.getElementById(`like-num${post.id}`).innerHTML
         post.querySelector('.likes').innerHTML = updatedLike;
       })
-      });
+    })
+  };
+
+  function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+  function unlikingStuff(post) {
+    let likeButton = post.querySelector(`.unlike-button`) 
+    likeButton.addEventListener('click', (event) => { 
+      event.preventDefault();
+      console.log(likeButton)
+      token = post.querySelector("input[name=authenticity_token]").value;
+      let formData = new FormData()
+      formData.append("_method", 'delete')
+      fetch(`posts/${post.id}/likes/${likeButton.id}`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': token
+        },
+        body: formData
+      })
+      .then(response => response.text())
+      .then(text => {
+        const parser = new DOMParser();
+        const htmlDocument = parser.parseFromString(text, "text/html");
+        console.log(htmlDocument)
+        updatedLike = htmlDocument.getElementById(`like-num${post.id}`).innerHTML
+        updatedLikeButton = htmlDocument.getElementById(`like-button${post.id}`)
+        console.log(updatedLikeButton)
+        insertAfter(likeButton, updatedLikeButton)
+        likeButton.remove();
+        post.querySelector('.likes').innerHTML = updatedLike;
+      })
     });
-  });
+  }
+});
