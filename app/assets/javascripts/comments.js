49a073c665836dbbd8aae0e8be4943e48deb1ef2
karimabuseer <karimabuseer@gmail.com>
2021-08-27 13:22:43 +0100

    Async comment loading done

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const posts = document.querySelectorAll('.post');
+
+  function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
   posts.forEach((post) => {
     post.querySelector('.comment-button').addEventListener('click', (e) => {
-        console.log("Output")
-        console.log(post)
-      commentBox = post.querySelector(".comment-content")
-      token = commentBox.querySelector("input[name=authenticity_token]").value; 
+    let commentBox = post.querySelector(".comment-content")
+    let commentList = post.querySelector(`#comment${post.id}`)
+    let commentText = post.querySelector("#comment_content").value
+    let token = commentBox.querySelector("input[name=authenticity_token]").value; 
       console.log(token)
-      console.log(commentBox.name)
       e.preventDefault()
+      formData = new FormData()
+      formData.append('comment[content]', commentText)
+      formData.append('commit', 'Create Comment')
       fetch(`posts/${post.id}/comments`, {
         method: 'POST',
         credentials: 'same-origin',
@@ -16,18 +22,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
           'X-Requested-With': 'XMLHttpRequest',
           'X-CSRF-Token': token
         },
-        data: {
-        "comment[content]": commentBox.name,
-        "commit": "Create Comment"  
-        }
+        body: formData
       })
       .then(response => response.text())
       .then(text => {
         const parser = new DOMParser();
         const htmlDocument = parser.parseFromString(text, "text/html");
-        const updatedComment = htmlDocument.documentElement.querySelector(".comment-content").innerHTML;
-        post.querySelector('.comment-content').innerHTML = updatedComment;
-        console.log("Output")
+        const updatedComment = htmlDocument.documentElement.querySelector(`#comment${post.id}`).lastElementChild;
+        commentList.appendChild(updatedComment);
+        post.querySelector("#comment_content").value = ""
+        console.log(updatedComment);
       });
     });
   });
