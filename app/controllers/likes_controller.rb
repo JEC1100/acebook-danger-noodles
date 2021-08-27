094eea1e6978d4688587e598319da3b6b5e5c527
karimabuseer <karimabuseer@gmail.com>
2021-08-27 13:40:01 +0100

    Code review changes. Mostly code styling

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,8 @@ class LikesController < ApplicationController
   before_action :find_like, only: [:destroy]
 
   def create
-    if already_liked?
-      Like.destroy(:post_id => params[:post_id], :user_id => current_user.id)
-      # flash[:notice] = "You can't like more than once"
-      redirect_to posts_url
-    else
-      Like.create(:post_id => params[:post_id], :user_id => current_user.id)
-      redirect_to posts_url
-    end
+    Like.create(:post_id => params[:post_id], :user_id => current_user.id)
+    redirect_to posts_url
   end
 
   def find_like
@@ -19,23 +13,12 @@ class LikesController < ApplicationController
  end
 
  def destroy
-  if (already_liked?)
     @like.destroy
     redirect_to posts_url
   end
-end
 
   private
   def find_post
     @post = Post.find(params[:post_id])
-  end
-
-  # Like.create(:post_id => params[:post_id], :user_id => current_user.id)
-  
-
-  def already_liked?
-  Like.where(user_id: current_user.id, post_id:params[:post_id]).exists?
-  end
-
-
+  end  
 end
